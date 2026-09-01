2d31f6cb1a61e76a3f1e90c791b62d07a0702d9a
APKMAX <enrique840914@gmail.com>
2026-09-01 13:54:00 -0400

    Update background_service.py

--- background_service.py
+++ background_service.py
@@ -1,25 +1,76 @@
 import time
 import traceback
+from jnius import autoclass
 
 
 TOTAL = 0
+CICLO = 0
+
+
+def actualizar_notificacion(texto):
+    try:
+        PythonService = autoclass(
+            "org.kivy.android.PythonService"
+        )
+
+        servicio = PythonService.mService
+
+        if servicio:
+            servicio.updateNotification(texto)
+
+    except Exception as e:
+        print(
+            "[NOTIFICACION ERROR]",
+            e,
+            flush=True
+        )
 
 
 def ejecutar_trabajo():
+
     global TOTAL
+    global CICLO
 
-    TOTAL += sum(range(1, 101))
+    CICLO += 1
 
-    print(f"[SERVICE] Total acumulado: {TOTAL}", flush=True)
+    # Trabajo de prueba
+    suma = sum(range(1, 101))
+
+    TOTAL += suma
 
 
-print("[SERVICE] Servicio iniciado", flush=True)
+    mensaje = (
+        f"Ciclo: {CICLO} | "
+        f"Suma: {suma} | "
+        f"Total: {TOTAL}"
+    )
+
+
+    print(
+        "[SERVICE]",
+        mensaje,
+        flush=True
+    )
+
+
+    actualizar_notificacion(
+        mensaje
+    )
+
+
+print(
+    "[SERVICE] Servicio iniciado",
+    flush=True
+)
+
 
 while True:
+
     try:
         ejecutar_trabajo()
 
     except Exception:
         traceback.print_exc()
 
-    time.sleep(30)
+
+    time.sleep(5)
